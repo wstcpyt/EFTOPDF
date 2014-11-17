1d6dbf88c149b0a4960e09effe9ef3b45c1374ab
wstcpyt <wstcpyt@gmail.com>
2014-11-17 17:01:58 -0500

    yutong

--- refractive.py
+++ refractive.py
@@ -1,7 +1,7 @@
 from scipy.interpolate import interp1d
 import numpy as np
 #extended refrative index from database
-Ag_k = np.genfromtxt('oc_si/Ag-k.txt', delimiter='\t',invalid_raise=False)
+Ag_k = np.genfromtxt('oc_si/glass-k.txt', delimiter='\t',invalid_raise=False)
 ry = np.array([])
 for object in Ag_k:
     ry = np.append(ry,object[1])
@@ -9,6 +9,8 @@ rx = np.array([])
 for object in Ag_k:
     rx = np.append(rx,object[0])
 fr = interp1d(rx, ry,kind='cubic')
-rxx = np.linspace(300,1100, 81)
+rxx = np.linspace(300,1200, 91)
 ryy = fr(rxx)
+np.savetxt('newx.txt',rxx)
+np.savetxt('newy.txt',ryy)
 print(ryy)
